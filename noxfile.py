01b9af06a71f0a5329f4f0ff840e3f1c695c1ebf
Josh Finch <me@joshfinch.com>
2020-04-02 13:10:33 +0100

    Moved test and packaging responsbility to nox

---
+++ noxfile.py
@@ -0,0 +1,9 @@
+# noxfile.py
+import nox
+
+
+@nox.session(python=["3.8"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", "--cov")
